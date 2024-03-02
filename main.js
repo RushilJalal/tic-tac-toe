ebad8aed33efa0a8a737cd40be56605da4fbb945
RushilJalal <rushiljalal@gmail.com>
2024-03-02 16:04:00 +0530

    delete everything

--- main.js
+++
@@ -1,6 +0,0 @@
-import './style.css';
-import ticTacToeModule from "./src/tictactoe";
-import gameFlowControllerModule from "./src/gameflowcontroller";
-import DOMControllerModule from "./src/DOMController";
-
-DOMControllerModule.handleSquareClick();
