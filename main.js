bee35d7a0606f56dece9dbb32d4c7e1b01412ac8
RushilJalal <rushiljalal@gmail.com>
2024-03-02 16:06:58 +0530

    final

---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css';
+import ticTacToeModule from "./src/tictactoe";
+import gameFlowControllerModule from "./src/gameflowcontroller";
+import DOMControllerModule from "./src/DOMController";
+
+DOMControllerModule.handleSquareClick();
